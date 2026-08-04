e42609c7f0d2314ea17695628c2c65ef0c8afdc1
abhaydv77 <abhayrox7575@gmail.com>
2026-08-04 12:51:05 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,6 @@
 import { betterAuth } from "better-auth";
 import Database from "better-sqlite3";
-import { nextCookies } from "better-auth/next-js";
-
-const db = new Database("auth.db");
 
 export const auth = betterAuth({
-  database: db,
-  plugins: [nextCookies()],
-});
+    database: new Database("./sqlite.db"),
+})
